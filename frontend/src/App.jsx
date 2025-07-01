214caf4b5ea1301017042b7f1376f807f2a76243
PhoeniX <rpx03775@gmail.com>
2025-07-01 21:30:47 +0530

    added basic structure for the frontend routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Dashboard from './components/Dashboard'
+import Login from './components/Login'
+import SignUp from './components/SignUp'
+import Profile from './components/Profile'
 
 function App() {
   return (
     <div>
-      App      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
   )
 }
